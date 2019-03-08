094d43156a577f2b1f8144745a812e624f5d4ca8
fatlard1993 <fatlard1993@gmail.com>
2019-03-08 15:44:43 -0800

    cleanup

--- src/sendPage.js
+++ src/sendPage.js
@@ -7,14 +7,14 @@ const pageCompiler = require('./pageCompiler');
 
 module.exports = function sendPage(name, status){
 	return function(req, res){
-    res.writeHead(status || 200, {'Content-Type': 'text/html', 'Content-Encoding': 'gzip'});
+		res.writeHead(status || 200, {'Content-Type': 'text/html', 'Content-Encoding': 'gzip'});
 
 		var start = now();
 
 		zlib.gzip(Buffer.from(pageCompiler.compile(name), 'utf8'), (_, result) => {
 			log(`Time to prepare "${name}": ${((now() - start) / 1000).toFixed(2)}s`);
 
-      res.end(result);
-    });
+			res.end(result);
+		});
 	};
 };
