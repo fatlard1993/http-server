a2c17160c39a1f897e5d7cebe5bf8cd7d973c8ec
fatlard1993 <fatlard1993@gmail.com>
2019-10-07 14:57:29 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const log = require('log');
 
-const sendPage = require('./sendPage');
-const pageCompiler = require('page-compiler');
 const onError = require('./error');
 const responsePrepper = require('./responsePrepper');
 const redirectTrailingWak = require('./redirectTrailingWak');
@@ -14,13 +12,13 @@ const app = polka({ onError });
 
 const httpServer = module.exports = {
 	app,
-	pageCompiler,
 	staticServer,
-	sendPage,
 	init: function(port, rootFolder, homePath = '/home'){
 		log(`[http-server] Starting from ${rootFolder} @ port "${port}" with the home path "${homePath}"`);
 
 		process.env.ROOT_FOLDER = rootFolder;
+
+		httpServer.pageCompiler = require('page-compiler');
 
 		app.use(responsePrepper, redirectTrailingWak(homePath), bodyParser.json(), bodyParser.urlencoded({ extended: false }), cookieParser());
 
