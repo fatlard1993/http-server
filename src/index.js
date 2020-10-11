1073c40e8c88a1bf935537776028e3f4ad9ff6f6
fatlard1993 <fatlard1993>
2020-10-11 14:59:34 -0700

    utilize new page compiler setOptions

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ const httpServer = module.exports = {
 	init: function(port, rootFolder, homePath = '/home'){
 		log(`[http-server] Starting from ${rootFolder} @ port "${port}" with the home path "${homePath}"`);
 
-		process.env.ROOT_FOLDER = rootFolder;
+		// process.env.ROOT_FOLDER = rootFolder;
 
-		httpServer.pageCompiler = require('page-compiler');
+		httpServer.pageCompiler = require('page-compiler').setOptions({ rootFolder });
 
 		app.use(responsePrepper, redirectTrailingWak(homePath), bodyParser.json(), bodyParser.urlencoded({ extended: false }), cookieParser());
 
